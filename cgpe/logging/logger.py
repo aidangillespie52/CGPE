898310805b34c0983e58046643ea4ab827842844
Aidan Gillespie <aidangillespie52@gmail.com>
2026-02-08 13:24:31 -0600

    refactor(logging): changed the spacing of level name

--- cgpe/logging/logger.py
+++ cgpe/logging/logger.py
@@ -21,7 +21,7 @@ def setup_logger(
         return logger  # prevent duplicate handlers
 
     formatter = logging.Formatter(
-        "%(asctime)s | %(levelname)-8s | %(name)s | %(message)s"
+        "%(asctime)s |  %(levelname)s  | %(name)s | %(message)s"
     )
 
     # ---- console ----
