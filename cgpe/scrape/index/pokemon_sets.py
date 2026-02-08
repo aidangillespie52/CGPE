6d9dcd7cce529c6e6e605dcaeb43021166abc270
Aidan Gillespie <aidangillespie52@gmail.com>
2026-02-08 13:31:24 -0600

    chore: added temporary links to scrape

--- cgpe/scrape/index/pokemon_sets.py
+++ cgpe/scrape/index/pokemon_sets.py
@@ -1,9 +1,10 @@
 # cgpe/scrape/index/pokemon_sets.py
 
 set_links_to_scrape = [
+    "https://www.pricecharting.com/console/pokemon-trick-or-trade-2024",
     "https://www.pricecharting.com/console/pokemon-evolving-skies",
-    #"https://www.pricecharting.com/console/pokemon-scarlet-&-violet",
-    #"https://www.pricecharting.com/console/pokemon-obsidian-flames",
-    #"https://www.pricecharting.com/console/pokemon-crown-zenith",
-    #"https://www.pricecharting.com/console/pokemon-scarlet-&-violet-151"
+    "https://www.pricecharting.com/console/pokemon-scarlet-&-violet",
+    "https://www.pricecharting.com/console/pokemon-obsidian-flames",
+    "https://www.pricecharting.com/console/pokemon-crown-zenith",
+    "https://www.pricecharting.com/console/pokemon-scarlet-&-violet-151"
 ]
