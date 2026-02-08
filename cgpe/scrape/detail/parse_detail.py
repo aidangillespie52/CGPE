46dd6330bfdd3ed7c6bcc00165c2b20acc1aa590
Aidan Gillespie <aidangillespie52@gmail.com>
2026-02-07 20:03:48 -0600

    feat(detail): added distributions of past sold for each grade to Detail

--- cgpe/scrape/detail/parse_detail.py
+++ cgpe/scrape/detail/parse_detail.py
@@ -1,7 +1,7 @@
 # cgpe/scrape/detail/parse_detail.py
 
 from dataclasses import dataclass
-from typing import Optional, Dict, List
+from typing import Optional, Dict, List, Tuple
 import json
 import re
 from pprint import pformat
@@ -24,6 +24,10 @@ class Detail:
     pop: Optional[dict]
     graded_prices_by_grade: Dict[str, Optional[float]]
     grades_1_to_10: List[Optional[float]]
+    grade7_dist: Tuple[float, float]
+    grade8_dist: Tuple[float, float]
+    grade9_dist: Tuple[float, float]
+    grade10_dist: Tuple[float, float]
     ungraded_price: Optional[float] = None
 
     def __repr__(self) -> str:
@@ -43,6 +47,10 @@ class Detail:
             f"  pop={pop_s},\n"
             f"  graded_prices_by_grade={prices_s},\n"
             f"  grades_1_to_10={g10_s},\n"
+            f"  grade7_dist=(mean={self.grade7_dist[0]},stddev={self.grade7_dist[1]})\n"
+            f"  grade8_dist=(mean={self.grade8_dist[0]},stddev={self.grade8_dist[1]})\n"
+            f"  grade9_dist=(mean={self.grade9_dist[0]},stddev={self.grade9_dist[1]})\n"
+            f"  grade10_dist=(mean={self.grade10_dist[0]},stddev={self.grade10_dist[1]}\n)"
             ")"
         )
 
@@ -105,6 +113,53 @@ def extract_prices_table(soup: BeautifulSoup) -> Dict[str, Optional[float]]:
     log.info("Extracted %d graded price entries", len(out))
     return out
 
+def extract_ebay_tables(soup: BeautifulSoup):
+    def extract(div):
+        table = div.find("table")
+        
+        if not table:
+            return (None, None)
+
+        trs = table.find_all("tr")
+        prices = []
+
+        for tr in trs[1:]: # skip headers
+            td = tr.find_all("td", recursive=False)[3]
+            price = td.find("span")
+
+            if not price:
+                continue
+
+            price = clean_price_text(price.text)
+            price = float(price)
+            prices.append(price)
+
+        mean = sum(prices) / len(prices)
+        stddev = (sum([(x - mean) ** 2 for x in prices]) / (len(prices)-1)) ** 0.5
+
+        return (mean, stddev)
+    
+    res_dict = dict()
+
+    soup = soup.find("div", id="price_comparison")
+    soup = soup.find("div", class_="tab-frame")
+
+    grade7_div = soup.find("div", class_="completed-auctions-cib")
+    grade8_div = soup.find("div", class_="completed-auctions-new")
+    grade9_div = soup.find("div", class_="completed-auctions-graded")
+    grade10_div = soup.find("div", class_="completed-auctions-manual-only")
+
+    mean7, dv7 = extract(grade7_div)
+    mean8, dv8 = extract(grade8_div)
+    mean9, dv9 = extract(grade9_div)
+    mean10, dv10 = extract(grade10_div)
+
+    res_dict['7'] = (mean7, dv7)
+    res_dict['8'] = (mean8, dv8)
+    res_dict['9'] = (mean9, dv9)
+    res_dict['10'] = (mean10, dv10)
+
+    return res_dict
 
 def extract_pop_data(html: str) -> Optional[dict]:
     log.debug("Extracting population data")
@@ -122,7 +177,6 @@ def extract_pop_data(html: str) -> Optional[dict]:
         log.warning("Failed to decode population JSON")
         return None
 
-
 def extract_card_name(soup: BeautifulSoup) -> str:
     div = soup.find("div", id="full_details")
     if not div:
@@ -137,7 +191,6 @@ def extract_card_name(soup: BeautifulSoup) -> str:
     name = clean_name(h2.text)
     log.debug("Extracted card name: %s", name)
     return name
-
 
 def extract_card_num(soup: BeautifulSoup) -> str:
     td = soup.find("td", itemprop="model-number")
@@ -157,7 +210,7 @@ def extract_card_num(soup: BeautifulSoup) -> str:
 def map_prices_to_1_to_10(
     prices: Dict[str, Optional[float]]
 ) -> List[Optional[float]]:
-    log.debug("Mapping prices to grade 1–10 vector")
+    log.debug("Mapping prices to grade 1-10 vector")
 
     out: List[Optional[float]] = []
     for i in range(1, 10):
@@ -168,7 +221,6 @@ def map_prices_to_1_to_10(
 
 
 def parse_detail_page(
-    *,
     html: str,
     card_link: str,
     source_config: SourceConfig,
@@ -180,6 +232,7 @@ def parse_detail_page(
     pop = extract_pop_data(html)
     graded_prices_by_grade = extract_prices_table(soup)
     grades_1_to_10 = map_prices_to_1_to_10(graded_prices_by_grade)
+    grade_ebay_tables = extract_ebay_tables(soup)
 
     detail = Detail(
         ungraded_price=graded_prices_by_grade.get("ungraded"),
@@ -190,6 +243,10 @@ def parse_detail_page(
         pop=pop,
         graded_prices_by_grade=graded_prices_by_grade,
         grades_1_to_10=grades_1_to_10,
+        grade7_dist=grade_ebay_tables['7'],
+        grade8_dist=grade_ebay_tables['8'],
+        grade9_dist=grade_ebay_tables['9'],
+        grade10_dist=grade_ebay_tables['10']
     )
 
     log.info(
@@ -200,3 +257,20 @@ def parse_detail_page(
     )
 
     return detail
+
+async def example():
+    import aiohttp
+
+    link = "https://www.pricecharting.com/game/pokemon-destined-rivals/arven's-toedscool-109"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as resp:
+            html = await resp.text()
+    
+    from cgpe.scrape.sources.pokemon import POKEMON_PRICECHARTING
+
+    d = parse_detail_page(html, link, POKEMON_PRICECHARTING)
+    print(repr(d))
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(example())
