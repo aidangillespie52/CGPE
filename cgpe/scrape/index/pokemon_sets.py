a85025ef304f258e2ed22bdb73e42753c75e7461
Aidan Gillespie <aidangillespie52@gmail.com>
2026-02-07 21:21:23 -0600

    feat(index): added pokemon sets to scrape

---
+++ cgpe/scrape/index/pokemon_sets.py
@@ -0,0 +1,9 @@
+# cgpe/scrape/index/pokemon_sets.py
+
+set_links_to_scrape = [
+    "https://www.pricecharting.com/console/pokemon-evolving-skies",
+    #"https://www.pricecharting.com/console/pokemon-scarlet-&-violet",
+    #"https://www.pricecharting.com/console/pokemon-obsidian-flames",
+    #"https://www.pricecharting.com/console/pokemon-crown-zenith",
+    #"https://www.pricecharting.com/console/pokemon-scarlet-&-violet-151"
+]
