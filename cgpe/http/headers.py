034c3a70e019595b52120225d8fa3cdc1ea2efe1
Aidan Gillespie <aidangillespie52@gmail.com>
2026-02-20 19:38:30 -0600

    feat: added option to supply headers

--- cgpe/http/headers.py
+++ cgpe/http/headers.py
@@ -6,14 +6,14 @@ log = setup_logger(__name__)
 _gen = HeaderGenerator()
 
 
-def build_headers() -> dict[str, str]:
+def build_headers(extra: dict[str, str] | None = None) -> dict[str, str]:
     headers = _gen.generate()
 
-    # Normalize / override encodings
     headers["Accept-Encoding"] = "gzip, deflate"
-
-    # Optional consistency tweaks
     headers["Accept-Language"] = "en-US,en;q=0.9"
+
+    if extra:
+        headers.update(extra)
 
     log.debug(
         "Headers built: UA=%s | Accept-Encoding=%s",
